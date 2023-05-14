df838bec952e43f5e3008b6d20a93697638117ba
arefin <arefinnuman2000@gmail.com>
2023-05-15 02:16:24 +0600

    Deploying

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AdminModule } from './admin/admin.module';
 import { CustomerModule } from './customer/customer.module';
 import { DbModule } from './db/db.module';
@@ -8,19 +10,23 @@ import { SellerModule } from './seller/seller.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 4444,
+      host: 'containers-us-west-42.railway.app',
+      port: 5464,
       username: 'postgres',
-      password: 'postgress',
-      database: 'deshi_dukaan_db',
+      password: '1WL9E3WWL4OXyjoOvjSD',
+      database: 'railway',
       autoLoadEntities: true,
       synchronize: true,
     }),
-
     DbModule,
     AdminModule,
     CustomerModule,
     SellerModule,
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public/',
+    }),
   ],
   controllers: [],
   providers: [],
